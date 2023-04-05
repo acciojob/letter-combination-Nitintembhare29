3289e275cd85fa3b7f0dbc20b8f8df7857ab1322
Nitintembhare29 <115885821+Nitintembhare29@users.noreply.github.com>
2023-04-05 13:03:42 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,38 +1,32 @@
-function letterCombinations(input_digit) {
-  //Complete the function
-	const map = {
-    '2': 'abc',
-    '3': 'def',
-    '4': 'ghi',
-    '5': 'jkl',
-    '6': 'mno',
-    '7': 'pqrs',
-    '8': 'tuv',
-    '9': 'wxyz'
-  };
-
-  let result = [];
-
-  if (digits.length === 0) {
-    return result;
+function letterCombinations(digits) {
+  if (!digits) {
+    return [];
   }
 
-  function backtrack(combination, nextDigits) {
-    if (nextDigits.length === 0) {
-      result.push(combination);
-    } else {
-      const digit = nextDigits[0];
-      const letters = map[digit];
-      for (let i = 0; i < letters.length; i++) {
-        const letter = letters[i];
-        backtrack(combination + letter, nextDigits.slice(1));
-      }
+  const map = {
+    2: 'abc',
+    3: 'def',
+    4: 'ghi',
+    5: 'jkl',
+    6: 'mno',
+    7: 'pqrs',
+    8: 'tuv',
+    9: 'wxyz'
+  };
+
+  function backtrack(digits, index, current, result) {
+    if (index === digits.length) {
+      result.push(current);
+      return;
+    }
+
+    const letters = map[digits[index]];
+    for (let i = 0; i < letters.length; i++) {
+      backtrack(digits, index + 1, current + letters[i], result);
     }
   }
 
-  backtrack('', digits);
-
+  const result = [];
+  backtrack(digits, 0, '', result);
   return result.sort();
 }
-
-module.exports = letterCombinations;
